1ef6cf23bb2976ec563ab76ebaf571513492043f
Jason Tuttle <jason@hearspotbark.com>
2017-06-02 09:52:49 -0400

    fixed an html tag and added header to README

--- new_project.sh
+++ new_project.sh
@@ -3,8 +3,8 @@
 mkdir project
 cd project
 touch index.html styles.css README.md
-echo "<link rel=\"stylesheet\" href=\"styles.css\"><h1>Hello World!</h2>" > index.html
+echo "<link rel=\"stylesheet\" href=\"styles.css\"><h1>Hello World!</h1>" > index.html
 echo "h1{color:red;font-family:sans-serif;text-decoration:underline;}" > styles.css
-echo "Hello World!" > README.md
+echo "# Hello World!" > README.md
 read -p "Press [ENTER] to continue..."
 echo "Congratulations!"
